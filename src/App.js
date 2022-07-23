c72a7a139a465e8211c120e926e224ee9ebda249
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2022-07-22 22:08:42 -0700

    building placeholder UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
+    <div className="app">
+      <div className="wrapper">
+        <div className="location">
+          <p>Chicago</p>
+        </div>
+        <div className="temperature">
+          <p>80&#176;F</p>
+        </div>
+        <div className="current_weather">          
+          <div className="description">
+            <p>Sunny</p>
+          </div>
+          <div className="humidity">
+            <p>50%</p>
+          </div>
+          <div className="wind">
+            <p>5 MPH</p>
+          </div>
+        </div>
+        <div className="pollution">Pollution: 19 AQI</div>
+        <div className="forecast">
+          This week's forecast:
+        </div>
+      </div>
     </div>
   );
 }
