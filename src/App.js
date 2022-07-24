ccbae2885f30d41597526daf9316e673762f623f
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2022-07-24 09:47:17 -0700

    Getting initial data from weather API

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
 import React, {useState} from 'react';
 
+const api = {
+  key: "1926eb0d297a53f9692631ce9c5436c8",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if(evt.key ==="Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+      });
+    }
+  }
+
   return (
     <div className="app">
-      <div className="wrapper">
-        <div className="location">
-          <p>Chicago</p>
-        </div>
-        <div className="temperature">
-          <p>80&#176;F</p>
-        </div>
-        <div className="current_weather">          
-          <div className="description">
-            <p>Sunny</p>
-          </div>
-          <div className="humidity">
-            <p>50%</p>
-          </div>
-          <div className="wind">
-            <p>5 MPH</p>
-          </div>
-        </div>
-        <div className="pollution">Pollution: 19 AQI</div>
-        <div className="forecast">
-          This week's forecast:
-        </div>
+      <div className="search_box">
+        <input 
+          type="text"
+          placeholder="Enter zip or city"
+          onChange={e => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+        />
       </div>
+      
+      <div className="wrapper">
+        {(typeof weather.main !=="undefined") ? (
+          <div>
+            <div className="location">{weather.name}</div>       
+            <div className="temperature">{Math.round(weather.main.temp)}&#176;C</div>
+            <div className="current_weather">          
+              <div className="description">
+                <p className="param">Sky</p>
+                {weather.weather[0].main}
+              </div>
+              <div className="humidity">
+                <p className="param">Humidity</p>
+                {Math.round(weather.main.humidity)}% 
+              </div>
+              <div className="wind">
+                <p className="param">Wind</p>
+                {weather.wind.speed} m/s
+              </div>
+            </div>
+            <div className="pollution">Pollution: 19 AQI</div>
+            <div className="forecast">
+              This week's forecast:
+            </div>
+          </div>
+        ) : ('')} 
+      </div>
+      
     </div>
   );
 }
